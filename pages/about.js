d3aeb8e5dc88ffa013dd23e6e76daf93a93560c4
Tausif Ahmed <tausifahmed471@gmail.com>
2022-07-04 07:08:19 +0600

    pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+
+    <div>This is the new about page</div>
+    
+  )
+}
+
+export default About;
---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+
+    <div>This is the new blog page</div>
+    
+  )
+}
+
+export default Blog;
