649c1eb4b7231272e2354def6199dfd21f75b1bd
Tausif Ahmed <tausifahmed471@gmail.com>
2022-07-04 09:00:35 +0600

    jsx fixes

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,9 @@ import '../styles/globals.css'
 import '../styles/style.css'
 
 function MyApp({ Component, pageProps }) {
-  console.log("this is _app.js");
+
   return <Component {...pageProps} />
+
 }
 
 export default MyApp
