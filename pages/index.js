cda4aa35378b07048e3d2759ddeefa3ad1171271
Tausif Ahmed <tausifahmed471@gmail.com>
2022-07-07 11:11:13 +0600

    adding Navbar to all pages

--- pages/index.js
+++ pages/index.js
@@ -29,35 +29,7 @@ export default function Home() {
       </Head>
 
 
-      <nav className={styles.mainnav}>
-        <ul>
-
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/blog">
-              <a>Blog</a>
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/contact">
-              <a>Contact</a>
-            </Link>
-          </li>
-
-        </ul>
-      </nav>
+      
 
       <main className={styles.main}>
         <h1 className={styles.title}>
